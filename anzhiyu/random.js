3428a67f94e525aabbb731e3299d5208c39d52ce
1320503154 <1320503154@qq.com>
2024-08-20 18:26:35 +0800

    Site updated: 2024-08-20 18:26:34

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["post/8a81cf60.html","post/95f630a1.html","post/95f630e6.html","post/7a5s6x2a.html","post/6c39bdac.html","post/6bcb6b2a.html","post/10acbf84.html","post/99b27ad4.html","post/99b2lhg6.html","post/566092e4.html","post/7acn6b2a.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
